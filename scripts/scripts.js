92d4273a95358e9a752877e78bf9833ab8c22b21
ashwin <alexwin75@gmail.com>
2020-08-05 04:00:36 -0400

    pushing accessibility changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,6 @@ radMovieQuiz.endDate = new Date("1999-12-31");
 
 radMovieQuiz.nonNinetiesAPIResults = []; //non90s movie results (promises) from API Call stored here
 radMovieQuiz.ninetiesAPIResults = []; //90s movie results (promises) from API Call stored here
-
 radMovieQuiz.nonNinetiesMovieArray = []; //non90s movie array
 radMovieQuiz.NinetiesMovieArray = []; //90s movie array
 
@@ -23,13 +22,6 @@ radMovieQuiz.nonNinetiesURLArray = [];
 radMovieQuiz.ninetiesURl = `https://api.themoviedb.org/3/discover/movie?api_key=${radMovieQuiz.apikey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&primary_release_date.lte=1999-12-31&primary_release_date.gte=1990-01-01&with_original_language=en&page=`;
 radMovieQuiz.ninetiesURLArray = [];
 
-//helper method to append page number to 90s movies API call parameters
-radMovieQuiz.generateNinetiesURLArray = (baseUrl, numPages) => {
-  for (let i = 1; i < numPages + 1; i++) {
-    radMovieQuiz.ninetiesURLArray.push(baseUrl + i);
-  }
-};
-
 //method to connect to The Movies DB API and retrieve 20 movies i.e 1 page
 radMovieQuiz.movieApiCall = function (apiUrl) {
   return $.ajax({
@@ -41,7 +33,10 @@ radMovieQuiz.movieApiCall = function (apiUrl) {
 
 //Promises for non Nineties movies API calls (movies from 80s and 2000s)
 radMovieQuiz.getNonNinetiesPromises = function () {
-  radMovieQuiz.nonNinetiesURLArray = [radMovieQuiz.eightiesUrl, radMovieQuiz.oughtsUrl];
+  radMovieQuiz.nonNinetiesURLArray = [
+    radMovieQuiz.eightiesUrl,
+    radMovieQuiz.oughtsUrl,
+  ];
   //retrieve '80s and '00s movies from the api and push promises to an array
   for (let i = 0; i < radMovieQuiz.nonNinetiesURLArray.length; i++) {
     radMovieQuiz.nonNinetiesAPIResults.push(
@@ -95,16 +90,6 @@ radMovieQuiz.GetMovies = async () => {
     });
 };
 
-//input: number of random numbers requested, Max number Output: array of one or more random numbers
-radMovieQuiz.randomIndexGenerator = (howMany, movieArrayLength) => {
-  let arrayofIndexes = [];
-  while (arrayofIndexes.length < howMany) {
-    let r = Math.floor(Math.random() * movieArrayLength);
-    if (arrayofIndexes.indexOf(r) === -1) arrayofIndexes.push(r);
-  }
-  return arrayofIndexes;
-};
-
 //input: number of random movies requested along with the list
 //output: returns requested random movies from a list
 radMovieQuiz.getRandomMovies = (numMovies, sourceArray) => {
@@ -141,66 +126,69 @@ radMovieQuiz.scrollAway = function (from, to) {
   });
 };
 
-//input: two arrays with correct and incorrect movies list
-//output: write to page
+//write movies to page
 radMovieQuiz.displayQuiz = () => {
-  let ninetiesMovies = radMovieQuiz.getRandomMovies(3, radMovieQuiz.ninetiesMovieArray);
-  let nonNinetiesMovies = radMovieQuiz.getRandomMovies(1, radMovieQuiz.nonNinetiesMovieArray);
+  let ninetiesMovies = radMovieQuiz.getRandomMovies(
+    3,
+    radMovieQuiz.ninetiesMovieArray
+  );
+  let nonNinetiesMovies = radMovieQuiz.getRandomMovies(
+    1,
+    radMovieQuiz.nonNinetiesMovieArray
+  );
   let arrayMovies = radMovieQuiz.shuffle([...ninetiesMovies, ...nonNinetiesMovies]); //shuffle the movies in the array
 
   if (arrayMovies.length == 4) {
     radMovieQuiz.finalFour = arrayMovies; //assigning the final four movies to a global object.
     $(".movieOptionOne input[type=image]").attr("src", radMovieQuiz.baseImageURL.concat(arrayMovies[0].poster_path));
-    $(".movieOptionOne input[type=image]").attr("alt", arrayMovies[0].title);
-    $(".movieOptionTwo input[type=image]").attr("src", radMovieQuiz.baseImageURL.concat(arrayMovies[1].poster_path));
-    $(".movieOptionTwo input[type=image]").attr("alt", arrayMovies[1].title);
-    $(".movieOptionThree input[type=image]").attr("src", radMovieQuiz.baseImageURL.concat(arrayMovies[2].poster_path));
-    $(".movieOptionThree input[type=image]").attr("alt", arrayMovies[2].title);
-    $(".movieOptionFour input[type=image]").attr("src", radMovieQuiz.baseImageURL.concat(arrayMovies[3].poster_path));
-    $(".movieOptionFour input[type=image]").attr("alt", arrayMovies[3].title);
+    $(".movieOptionOne input[type=image]").attr("alt", "Is this a 90s movie?: " + arrayMovies[0].title);
 
+    $(".movieOptionTwo input[type=image]").attr("src", radMovieQuiz.baseImageURL.concat(arrayMovies[1].poster_path));
+    $(".movieOptionTwo input[type=image]").attr("alt", "Is this a 90s movie?: " + arrayMovies[1].title);
+
+    $(".movieOptionThree input[type=image]").attr("src", radMovieQuiz.baseImageURL.concat(arrayMovies[2].poster_path));
+    $(".movieOptionThree input[type=image]").attr("alt", "Is this a 90s movie?: " + arrayMovies[2].title);
+
+    $(".movieOptionFour input[type=image]").attr("src", radMovieQuiz.baseImageURL.concat(arrayMovies[3].poster_path));
+    $(".movieOptionFour input[type=image]").attr("alt", "Is this a 90s movie?: " + arrayMovies[3].title);
   }
 };
 
-//helper function to identify the index of the correct answer
-radMovieQuiz.getAnswerIndex = function () {
-  let selectedReleaseDate;
-  for (let i = 0; i < radMovieQuiz.finalFour.length; i++) {
-    selectedReleaseDate = new Date(radMovieQuiz.finalFour[i].release_date);
-    if (
-      (selectedReleaseDate < radMovieQuiz.startDate) ||
-      (selectedReleaseDate > radMovieQuiz.endDate)
-    ) {
-
-      return i;
-    }
-  }
-}
-
+//highlight the correct option and also, the incorrect answer selected if any
 radMovieQuiz.styleRightWrong = () => {
   //first style the one in focus
+  $(`.movieOption:nth-of-type(${radMovieQuiz.selectedMovieIndex}) div`)
+    .text(
+      "❌ Released: " +
+      radMovieQuiz.finalFour[
+        radMovieQuiz.selectedMovieIndex - 1
+      ].release_date.split("-")[0]
+    )
+    .addClass("focusAnswer");
 
-  $(`.movieOption:nth-of-type(${radMovieQuiz.selectedMovieIndex}) div`).text("❌ Released: " + radMovieQuiz.finalFour[radMovieQuiz.selectedMovieIndex - 1].release_date.split('-')[0]).addClass("focusAnswer");
-  //next style the image that is correct 
+  //next style the image that is correct
   let answerIndex = radMovieQuiz.getAnswerIndex();
   let nthOfTypeValue = answerIndex + 1;
 
-  $(`.movieOption:nth-of-type(${nthOfTypeValue}) div`).text("✔️ Released: " + radMovieQuiz.finalFour[answerIndex].release_date.split('-')[0]).addClass("correctAnswer");;
-
-
+  $(`.movieOption:nth-of-type(${nthOfTypeValue}) div`)
+    .text(
+      "✔️ Released: " +
+      radMovieQuiz.finalFour[answerIndex].release_date.split("-")[0]
+    )
+    .addClass("correctAnswer");
 };
 
 radMovieQuiz.eventListener = function () {
   //event listener for the Get Started button
   radMovieQuiz.scrollAway(".btnStart", ".movieQuiz");
 
-  //when a movie is in focus save the selected movie
+  //when a movie is in focus, save the selected movie
   $(".movieOption input[type=image]").on("click", function (e) {
-
     e.preventDefault();
     if (radMovieQuiz.IsQuizOn) {
       radMovieQuiz.selectedMovieIndex = $(this).attr("index"); //values from 1 to 4
-      radMovieQuiz.selectedMovie = radMovieQuiz.finalFour[radMovieQuiz.selectedMovieIndex - 1];
+      radMovieQuiz.selectedMovie =
+        radMovieQuiz.finalFour[radMovieQuiz.selectedMovieIndex - 1];
       $(".btnSubmit").attr("disabled", false);
     }
   });
@@ -212,30 +200,39 @@ radMovieQuiz.eventListener = function () {
       //show correct / incorrect answers
       radMovieQuiz.styleRightWrong();
       radMovieQuiz.resetEverything();
-    }
-    else {
+    } else {
       //load new questions
       radMovieQuiz.loadNextQuestion();
     }
-
   });
 };
 
+//load questions - from the locally stored array (so no round-trip to the API required)
 radMovieQuiz.loadNextQuestion = () => {
   radMovieQuiz.IsQuizOn = true;
   radMovieQuiz.displayQuiz();
   $(".btnSubmit").text("Submit");
   $(".movieOption div").text("").removeClass();
-  $(".btnSubmit").prop('disabled', true);
-
-}
-
+  $(".btnSubmit").prop("disabled", true);
+};
 
 //reset Submit button
 radMovieQuiz.resetEverything = () => {
   $(".btnSubmit").text("Play Again");
   radMovieQuiz.IsQuizOn = false;
-}
+};
+
+//init function - on first load
+radMovieQuiz.init = async function () {
+  await radMovieQuiz.GetMovies(); //wait for all the results from the multiple API calls
+  radMovieQuiz.displayQuiz();
+  radMovieQuiz.eventListener();
+};
+
+//document ready function
+$(document).ready(function () {
+  radMovieQuiz.init();
+});
 
 //helper function to shuffle elements within an array (Fisher-Yates shuffle)
 //source: https://javascript.info/task/shuffle
@@ -248,21 +245,34 @@ radMovieQuiz.shuffle = (array) => {
   return array;
 };
 
-//init function - on first load
-radMovieQuiz.init = async function () {
-  await radMovieQuiz.GetMovies(); //wait for all the results from the multiple API calls
-  // let ninetiesMovies = radMovieQuiz.getRandomMovies(3, radMovieQuiz.ninetiesMovieArray);
-  // let nonNinetiesMovies = radMovieQuiz.getRandomMovies(1, radMovieQuiz.nonNinetiesMovieArray);
-  // let allmovies = radMovieQuiz.shuffle([...ninetiesMovies,...nonNinetiesMovies]); //shuffle the movies in the array
-  // radMovieQuiz.displayQuiz(allmovies);
-
-  radMovieQuiz.displayQuiz();
-
-  radMovieQuiz.eventListener();
+//helper function to identify the index of the correct answer
+radMovieQuiz.getAnswerIndex = function () {
+  let selectedReleaseDate;
+  for (let i = 0; i < radMovieQuiz.finalFour.length; i++) {
+    selectedReleaseDate = new Date(radMovieQuiz.finalFour[i].release_date);
+    if (
+      selectedReleaseDate < radMovieQuiz.startDate ||
+      selectedReleaseDate > radMovieQuiz.endDate
+    ) {
+      return i;
+    }
+  }
 };
 
-$(document).ready(function () {
-  radMovieQuiz.init();
-});
+//input: number of random numbers requested, Max number Output: array of one or more random numbers
+//helper function to generate a few random numbers
+radMovieQuiz.randomIndexGenerator = (howMany, movieArrayLength) => {
+  let arrayofIndexes = [];
+  while (arrayofIndexes.length < howMany) {
+    let r = Math.floor(Math.random() * movieArrayLength);
+    if (arrayofIndexes.indexOf(r) === -1) arrayofIndexes.push(r);
+  }
+  return arrayofIndexes;
+};
 
-
+//helper method to append page number to 90s movies API call parameters
+radMovieQuiz.generateNinetiesURLArray = (baseUrl, numPages) => {
+  for (let i = 1; i < numPages + 1; i++) {
+    radMovieQuiz.ninetiesURLArray.push(baseUrl + i);
+  }
+};
